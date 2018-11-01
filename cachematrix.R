9ed7dcf610680b8443a08827415087712099b40d
kisukhova <44554347+kisukhova@users.noreply.github.com>
2018-11-01 20:04:13 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,29 +3,33 @@
 
 ## Write a short comment describing this function
 ## makeCacheMatrix creates a list containing the function that sets the value of the matrix, we get the value of the matrix, the value of the inverse matrix, we get the value of the inverse matrix
-makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+
+makeCacheMatrix <- function(a = matrix()) {
+b <- NULL	
+get <- function() return(a)	
+getInverse <- function() return(b)	
+set <- function(v) {
+	a <<- v
+	inverse <<- NULL
+}	
+setInverse <- function(v) b <<- v	
+return(
+	list(
+		set = set,get = get,setInverse = setInverse,getInverse = getInverse
+		)	
+	)
 }
+
 
 ## Write a short comment describing this function
 ## The function returns the inverse matrix. First it checks if the inversion has already been calculated. If so, it gets the result and skips the calculation. If not, it calculates the reverse.
 
-cacheSolve <- function(x, ...) {
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("getting cached data.")
-        return(inv)
-    }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinverse(inv)
-    inv
+cacheSolve <- function(cs, ...) {
+b <- cs$getInverse()	
+if (!is.null(b)) 
+return(b)	
+a <- cs$get()
+b <- solve(a, ...)
+cs$setInverse(b)	
+	return(b)
 }
